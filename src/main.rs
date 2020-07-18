cf6d93f41efe3a94dbedd24819594133b9c594fb
corey <wilso.core@gmail.com>
2020-07-18 11:05:18 -0700

    adding some extra files

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,38 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream){
-    let mut buffer = [0; 512];
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n",  "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    }
+    
+    if buffer.starts_with(getfoo){
+        let contents = fs::read_to_string("hello.html").unwrap();
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-}
+    }
+    else {
+        // some other request
+        let status_line= "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
 
+        let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    }
+}
 
