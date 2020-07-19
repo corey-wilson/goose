40bcab353bcc3d24e04e9648a8e018967e762113
corey <wilso.core@gmail.com>
2020-07-18 17:53:49 -0700

    adding an all

--- src/main.rs
+++ src/main.rs
@@ -7,46 +7,34 @@ fn main() {
     let address = String::from("127.0.0.1:7878");
     let listener = TcpListener::bind(address).unwrap();
 
-    for stream in listener.incoming(){
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         handle_connection(stream);
     }
 }
 
-fn handle_connection(mut stream: TcpStream){
+fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
+    let home = b"GET / HTTP/1.1\r\n";
+    let all = b"GET /all HTTP/1.1\r\n";
 
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n",  "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
-    }
-    
-    if buffer.starts_with(getfoo){
-        let contents = fs::read_to_string("hello.html").unwrap();
-
-        let response = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-            contents.len(),
-            contents
-        );
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    let (status_line, filename) = if buffer.starts_with(home) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(all) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "all.html")
     }
     else {
-        // some other request
-        let status_line= "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        let contents = fs::read_to_string("404.html").unwrap();
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-        let response = format!("{}{}", status_line, contents);
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-    }
 }
+
 
